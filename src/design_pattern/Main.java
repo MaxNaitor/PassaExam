4dbfc881f4c49dcadac51a0c9af6623447d38cef
Tiziano Massa <tiziano.max@gmail.com>
2023-10-31 10:35:32 +0100

    gestione esercizi

--- src/design_pattern/Main.java
+++ src/design_pattern/Main.java
@@ -6,12 +6,16 @@ import design_pattern.strategy.AnatraVera;
 public class Main {
 
 	public static void main(String[] args) {
+		strategyPattern();
+	}
+
+	public static void strategyPattern() {
 		AnatraDiPlastica plastica = new AnatraDiPlastica();
 		AnatraVera vera = new AnatraVera();
-		
+
 		plastica.nuota();
 		vera.nuota();
-		
+
 		plastica.getComportamentoVolo().vola();
 		vera.getComportamentoVolo().vola();
 	}
